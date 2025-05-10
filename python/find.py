8664a70a1b56ebab3a185615b4bdf8336908406c
Andres Marquez <andresmr955@gmail.com>
2025-05-10 07:38:29 -0400

    String is inmutable

--- python/find.py
+++ python/find.py
@@ -1,7 +1,11 @@
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 text = 'Hello World'
+text[0] = 'P'
 shift = 3
 
 print(alphabet.find('z'))
 
 print(alphabet.find(text[0]))
+index = alphabet.find(text[0].lower())
+shifted = alphabet[index]
+print(shifted)
