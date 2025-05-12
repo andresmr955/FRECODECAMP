fda8ca063baa5bf4ac5de57c1869cbd7e59198e7
Andres Marquez <andresmr955@gmail.com>
2025-05-12 07:17:13 -0400

    refactor code

--- python/luhn.py
+++ python/luhn.py
@@ -1,27 +1,17 @@
 valid_number = "4539578763621486"
 not_valid_number = "4539548763621486"
 
-def luhn(number_client: int) -> str:
+def luhn(number_client: str) -> str:
 
     reversed_lst = list(reversed(number_client))
-    list_multiply = []
-    
-    list_not_modified = []
+    new_nine = list(map(lambda x: x - 9 if x > 9 else x, map(lambda i: int(reversed_lst[i]) * 2, range(1, len(reversed_lst), 2))))
 
-    for i in range(1, len(number_client), 2):
-        list_multiply.append(int(reversed_lst[i]) * 2)
-        new_nine = [ i - 9 if i > 9 else i for i in list_multiply]
-    
-    for j in range(0, len(number_client), 2):
-        list_not_modified.append(reversed_lst[j])
+    list_integers_one = list(map(lambda i: int(reversed_lst[i]), range(0, len(reversed_lst), 2)))
 
-    list_integers_one = list(map(int, list_not_modified))
-    list_integers_two = list(map(int, new_nine))
-
-    total_sum = sum(list_integers_one) + sum(list_integers_two)
+    total_sum = sum(list_integers_one) + sum(new_nine)
 
     return f'Your number is valid for a card' if total_sum % 10 == 0 else f'Your number is not valid'
-
     
 print(luhn(valid_number))
 print(luhn(not_valid_number))
+print(luhn('4111111145551142'))
