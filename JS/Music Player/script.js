96d7516151bc50e622429eea9de498a5ac9fec85
Andres Marquez <andresmr955@gmail.com>
2025-07-16 20:20:42 -0400

    ccreating my first song object

--- JS/Music Player/script.js
+++ JS/Music Player/script.js
@@ -6,4 +6,13 @@ const nextButton = document.getElementById('next');
 const previousButton = document.getElementById('previous');
 const shuffleButton = document.getElementById('shuffle');
 
-let allSongs = [];
+let allSongs = [
+    {
+        id: 0,
+        title: "Corazon Embustero",
+        artist: "Carlos Munoz", 
+        duration: "4:25", 
+        src:"https://www.youtube.com/watch?v=afDEunyLpyw&list=RDafDEunyLpyw&start_radio=1&ab_channel=CafeRecords" 
+
+    }
+];
