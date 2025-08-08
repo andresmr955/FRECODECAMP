3cf308dfbce137a0bfb9a67802a2877b45fb0fea
Andres Marquez <andresmr955@gmail.com>
2025-08-08 14:18:46 -0400

    craeting an if statemaent to check that the song it starts from the begining and if the current song is null or is diferent to the id

--- JS/Music Player/script.js
+++ JS/Music Player/script.js
@@ -132,6 +132,10 @@ renderSongs(sortSongs());
 
 const playSong = (id) => {
   const song = userData?.songs.find((song) => song.id === id )
-  audio.src = song.src
-  audio.title = song.title
+  audio.src = song.src;
+  audio.title = song.title;
+  if(userData?.currentSong === null || userData?.currentSong.id !== song.id ){
+      audio.currentTime = 0
+    }
+ 
 }
