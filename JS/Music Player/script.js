2cc9a0e2b8e04170b70b53c4c71b5e4e5282f86c
Andres Marquez <andresmr955@gnail.com>
2025-08-15 11:04:58 -0400

    creating contiditional to check if the song is playing

--- JS/Music Player/script.js
+++ JS/Music Player/script.js
@@ -134,4 +134,8 @@ const playSong = (id) => {
   const song = userData?.songs.find((song) => song.id === id )
   audio.src = song.src
   audio.title = song.title
+
+  if(userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+    audio.currentTime = 0;
+  }
 }
