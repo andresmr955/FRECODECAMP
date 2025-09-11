eaa08bdd2eabb130381765ef1dfebc308b6a4607
Andres Marquez <andresmr955@gmail.com>
2025-09-11 16:31:50 -0400

    making changes about the code beacuse the css was other color

--- JS/Music Player/script.js
+++ JS/Music Player/script.js
@@ -1,7 +1,6 @@
 const playlistSongs = document.getElementById('playlist-songs');
 const playButton = document.getElementById("play");
 const pauseButton = document.getElementById("pause");
-
 const nextButton = document.getElementById('next');
 const previousButton = document.getElementById('previous');
 const shuffleButton = document.getElementById('shuffle');
@@ -13,7 +12,6 @@ let allSongs = [
         artist: "Lukas Graham", 
         duration: "4:25", 
         src:"./mysongs/sevenyears.mp3" 
-
     },
     {
     id: 1,
@@ -80,15 +78,12 @@ let allSongs = [
   },
 ];
 
-
 const audio = new Audio();
-
 let userData = {
   songs: [...allSongs],
   currentSong: null,
-  songCurrentTime: 0
+  songCurrentTime: 0,
 };
-
 
 const playSong = (id) => {
   const song = userData?.songs.find((song) => song.id === id )
@@ -102,17 +97,42 @@ const playSong = (id) => {
   }
   userData.currentSong = song;
   playButton.classList.add('playing');
+
   highlightCurrentSong();
   audio.play();
-
+  highlightCurrentSong();
 };
 
+const pauseSong = () => {
+  userData.currentTime = audio.currentTime;
+  playButton.classList.remove('playing');
+  audio.pause();
+}
 
+
+const playNextSong = () => {
+if(userData?.currentSong === null){
+  playSong(userData?.songs[0].id)
+}
+else{
+  const currentSongIndex = getCurrentSongIndex();
+  const nextSong = userData?.songs[currentSongIndex + 1];
+  playSong(nextSong.id);
+}
+};
+
+const playPreviousSong = () => {
+  if(userData?.currentSong === null) return;
+  
+    const currentSongIndex = getCurrentSongIndex();
+    const previousSong = userData?.songs[currentSongIndex - 1];
+    playSong(previousSong.id);
+};
 const highlightCurrentSong = () => {
   const playlistSongElements = document.querySelectorAll(".playlist-song");
+  const songToHighlight = document.getElementById(`song- ${userData?.currentSong?.id}`
 
-  const songToHighlight = document.getElementById(`song- ${userData?.currentSong?.id
-}`);
+  );
   playlistSongElements.forEach((songEl) => {
     songEl.removeAttribute("aria-current");
   });
@@ -127,9 +147,11 @@ const renderSongs = (array) => {
     .map((song) => {
       return `
       <li id="song-${song.id}" class="playlist-song"> 
-
         <button class="playlist-song-info" onclick='playSong(${song.id})'>
           <span class="playlist-song-title">${song.title}</span>
+          <span class="playlist-song-artiste">${song.artist}</span>
+          <span class="playlist-song-duration">${song.duration}
+          </span>
         </button>
         <button class="playlist-song-delete"aria-label="Delete  ${song.title}">
             <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/><path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/>
@@ -142,6 +164,12 @@ const renderSongs = (array) => {
 };
 
 // renderSongs(userData?.songs);
+
+
+
+const getCurrentSongIndex = () => {
+  return userData?.songs.indexOf(userData?.currentSong);
+a}
 
 playButton.addEventListener("click", () => {
   if(userData?.currentSong === null) 
@@ -169,40 +197,15 @@ const sortSongs = () => {
 
 renderSongs(sortSongs());
 
-const pauseSong = () => {
-  userData.currentTime = audio.currentTime;
-  playButton.classList.remove('playing');
-  audio.pause();
-}
 
 pauseButton.addEventListener("click", pauseSong)
 
-const getCurrentSongIndex = () => {
-  return userData?.songs.indexOf(userData?.currentSong);
-a}
 
-const playNextSong = () => {
-if(userData?.currentSong === null){
-  playSong(userData?.songs[0].id)
-}
-else{
-  const currentSongIndex = getCurrentSongIndex();
-  const nextSong = userData?.songs[currentSongIndex + 1];
-  playSong(nextSong.id);
-}
-};s
+
 
 nextButton.addEventListener("click", playNextSong);
 
-const playPreviousSong = () => {
-  if(userData?.currentSong === null){
-    playSong(userData?.songs[0].id)
-  }else{
-    const currentSongIndex = getCurrentSongIndex();
-    const previousSong = userData?.songs[currentSongIndex - 1];
-    playSong(previousSong.id);
-  }
-};
+
 previousButton.addEventListener("click", playPreviousSong);
 
 
