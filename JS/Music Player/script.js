4459e806015890ef14f70078bce8775ff1022159
Andres Marquez <andresmr955@gmail.com>
2025-09-12 13:23:44 -0400

    creating and assigning the title and artist to the var

--- JS/Music Player/script.js
+++ JS/Music Player/script.js
@@ -138,8 +138,8 @@ const setPlayerDisplay = () => {
   const playingSong = document.getElementById("player-song-title");
   const songArtist = document.getElementById("player-song-artist");
 
-
-
+  const currentTitle = userData?.currentSong?.title;
+  const currentSong = userData?.currentSong?.artist;
 };
 
 const highlightCurrentSong = () => {
