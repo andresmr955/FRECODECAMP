dab791f21a0a95fcd1f5eae1bcb0d67b7d8fb2bf
Andres Marquez <andresmr955@gmail.com>
2025-10-06 16:51:21 -0400

    adding a child with append child

--- JS/Music Player/script.js
+++ JS/Music Player/script.js
@@ -209,9 +209,12 @@ const renderSongs = (array) => {
 
     if(userData?.songs.length === 0){
       const resetButton = document.createElement("button");
-      const resetText = document.createTextNode("Reset");
+      const resetText = document.createTextNode("Reset all Songs");
       resetButton.id = "reset";
       resetButton.ariaLabel = "Reset playlist";
+
+     resetButton.appendChild(resetText);
+     playlistSongs.appendChild(resetButton);
     }
 };
 
